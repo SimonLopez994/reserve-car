350545993335ad504e858db54ae18243b7db33d1
Simon <chimon994@gmail.com>
2024-10-02 19:47:34 -0300

    first commit

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route></Route>
+        </Routes>
+    )
+}
+
+export default App;
